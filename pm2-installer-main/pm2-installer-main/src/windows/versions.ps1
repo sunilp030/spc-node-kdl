2bce5632da472c85823b122dc4e03a648f322e6e
sunilp030 <52525579+sunilp030@users.noreply.github.com>
2026-04-21 19:11:40 +0530

    pm2-installer-main removed

--- pm2-installer-main/pm2-installer-main/src/windows/versions.ps1
+++
@@ -1,8 +0,0 @@
-$pm2_package = "$(node src/tools/echo-dependency.js pm2)"
-$pm2_logrotate_package = "$(node src/tools/echo-dependency.js @jessety/pm2-logrotate)"
-$node_windows_package = "$(node src/tools/echo-dependency.js node-windows)"
-
-Write-Host "Using:"
-Write-Host "- $pm2_package"
-Write-Host "- $pm2_logrotate_package"
-Write-Host "- $node_windows_package"
